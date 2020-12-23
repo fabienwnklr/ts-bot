e27c90c6b3be87ee13baaba2ca8d135671c053b2
Fabien Winkler <fabien.winkler@outlook.fr>
2020-12-23 19:32:44 +0100

    clear

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { botConfig } from '../types/bot-config';
 
 export const Config: botConfig = {
   token: process.env.BOT_TOKEN,
-  prefix: '>',
+  prefix: '!',
   botOwnerRoleName: 'bot-owner',
   enableReactions: true,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import 'dotenv/config';
-import Discord from 'discord.js';
+import Discord, { Message } from 'discord.js';
 import { Config } from './config';
+import { botConfig } from './types/bot-config';
+
+/** Pre-startup validation of the bot config. */
+function validateConfig(botConf: botConfig) {
+    if (!botConf.token) {
+        throw new Error('You need to specify your Discord bot token!');
+    }
+}
+
+validateConfig(Config);
 
 const client: Discord.Client = new Discord.Client();
+
 client.login(Config.token);
 
 client.on('ready', () => console.log('Bot has been started !'));
+
+client.on('message', (message: Message) => {
+    console.log(message);
+});
