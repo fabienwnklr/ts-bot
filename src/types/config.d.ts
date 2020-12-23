ddce4bf0b5e92b4598f8ce8c7df092bcc3a37482
Fabien Winkler <fabien.winkler@outlook.fr>
2020-12-23 18:23:43 +0100

    first commit

---
+++ src/types/config.d.ts
@@ -0,0 +1,18 @@
+export interface Config {
+    env: string;
+    token: string;
+    discordBotToken: string;
+    owner: string;
+    prefix: string;
+    db: {
+      host: string;
+      port: string;
+      driver?: string;
+      name: string;
+      user: string;
+      password: string;
+    },
+    test?: {
+      length: number;
+    },
+  }
